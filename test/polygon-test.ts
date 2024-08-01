9ef7945fee3284de408788a3726009035f8dc6a3
Philipp Legner <plegner@amplify.com>
2024-08-01 11:18:31 +0100

    Remove polygon clipping code

--- test/polygon-test.ts
+++ test/polygon-test.ts
@@ -9,8 +9,6 @@ import {Line, Point, Polygon, Polyline} from '../src';
 
 
 const poly = (...p: number[][]) => new Polygon(...p.map(q => new Point(q[0], q[1])));
-const line = (...p: number[][]) => new Line(new Point(p[0][0], p[0][1]), new Point(p[1][0], p[1][1]));
-const points = (p: Polygon) => p.points.map(q => q.array);
 
 
 tape('Length and Circumference', (test) => {
@@ -31,24 +29,6 @@ tape('Convex Hull', (test) => {
   test.deepEquals(hull.points[0], points[4]);
   test.deepEquals(hull.points[1], points[2]);
   test.deepEquals(hull.points[2], points[3]);
-
-  test.end();
-});
-
-tape('Cutting', (test) => {
-  const shape = poly([0, 0], [2, 0], [2, 1], [1, 1], [1, 2], [0, 2]);
-
-  const c1 = shape.cut(line([1, 0], [1, 1])).map(p => points(p));
-  test.deepEquals(c1, [[[2, 1], [2, 0], [1, 0], [1, 1]], [[1, 2], [1, 0], [0, 0], [0, 2]]]);
-
-  const c2 = shape.cut(line([-1, 1], [0, 1])).map(p => points(p));
-  test.deepEquals(c2, [[[2, 1], [2, 0], [0, 0], [0, 1]], [[1, 2], [1, 1], [0, 1], [0, 2]]]);
-
-  const c3 = shape.cut(line([0, 0], [1, 1])).map(p => points(p));
-  test.deepEquals(c3, [[[2, 1], [2, 0], [0, 0], [1, 1]], [[1, 2], [1, 1], [0, 0], [0, 2]]]);
-
-  const c4 = shape.cut(line([0, 2], [2, 0])).map(p => points(p));
-  test.deepEquals(c4, [[[2, 0], [0, 0], [0, 2]], [[0, 2], [1, 1], [1, 2]], [[1, 1], [2, 0], [2, 1]]]);
 
   test.end();
 });
